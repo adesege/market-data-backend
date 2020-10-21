79689cfe1f26f4d20e7ce19ec96a99cca149ee04
Fadojutimi Temitayo Olusegun <Fadojutimitemitayo@gmail.com>
2020-10-21 23:55:26 +0100

    ch: refactor signin controller

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt/dist/jwt.service';
 import { InjectModel } from '@nestjs/sequelize';
+import { Op } from 'sequelize';
+import { IRoles } from 'src/interfaces/role';
 import User from 'src/user/models/user';
 import { BcryptService } from '../../services/bcrypt.service';
 
@@ -14,19 +16,24 @@ export class AuthService {
 
   async validateUser(email: string, password: string): Promise<Partial<User>> {
     const user = await this.userModel.findOne({
-      where: { email: email },
-      attributes: ['email', 'password', 'id', 'firstName', 'lastName']
+      where: {
+        email: email,
+        roles: {
+          [Op.contains]: [IRoles.ADMIN]
+        }
+      },
+      attributes: ['email', 'password', 'id', 'firstName', 'lastName', 'roles']
     });
     if (!user) return null;
 
     const isEqual = await this.bcryptService.compare(password, user.password);
     if (isEqual) {
-      return { ...user, password: undefined };
+      return { ...user.get(), password: undefined };
     }
     return null;
   }
 
-  signToken(user: User): Promise<string> {
+  signToken(user: Partial<User>): Promise<string> {
     const payload = {
       email: user.email,
       firstName: user.firstName,
